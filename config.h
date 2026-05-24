fd9ba285fef5a5f3ac77286ba7f0cf1754845db0
Gurov <103680464+Gur0v@users.noreply.github.com>
2026-05-24 17:31:19 +0300

    Create config.h

---
+++ config.h
@@ -0,0 +1,13 @@
+static const struct idle_stage {
+    unsigned int  timeout_ms;
+    const char    *cmd_idle;
+    const char   *cmd_resume;
+} idle_stages[] = {
+    {  10000, "wlopm --off \"*\"", "wlopm --on \"*\"" },
+ // { 300000, "systemctl suspend", NULL               },
+};
+
+#define CMD_PRE_LOCK    NULL
+#define CMD_POST_UNLOCK NULL
+
+#define DEFAULT_LOCKER_ARGS "swaylock", "-c", "000000", "--font", "IBM Plex Sans"
